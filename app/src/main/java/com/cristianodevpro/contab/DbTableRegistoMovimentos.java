d2f73593293d6824114e2fef8bbde1d158a1bc75
Cristiano Patrício <cristianop1998@gmail.com>
2018-05-31 21:33:23 +0100

    Criação do método getContentValues [DbTableRegistoMovimentos] Criação do método insert para inserir os dados na BD

--- app/src/main/java/com/cristianodevpro/contab/DbTableRegistoMovimentos.java
+++ app/src/main/java/com/cristianodevpro/contab/DbTableRegistoMovimentos.java
@@ -1,5 +1,6 @@
 package com.cristianodevpro.contab;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
@@ -25,20 +26,39 @@ public class DbTableRegistoMovimentos implements BaseColumns {
     //Criação da tabela
     public void create(){
         db.execSQL(
-                "CREATE TABLE " + TABLE_NAME + " (" +
-                ID_MOVIMENTO + " TEXT PRIMARY KEY," +
-                DIA + " INTEGER NOT NULL," +
-                MES + " INTEGER NOT NULL," +
-                ANO + " INTEGER NOT NULL," +
-                RECEITADESPESA + " TEXT NOT NULL," +
-                DESGINACAO + " TEXT NOT NULL," +
-                VALOR + " REAL NOT NULL," +
-                FK_ID_RECEITA + " TEXT," +
-                        "FOREIGN KEY ("+FK_ID_RECEITA+") REFERENCES "+DbTableTipoReceita.TABLE_NAME+"("+DbTableTipoReceita.ID_RECEITA+")" +
-                        FK_ID_DESPESA + " TEXT," +
-                        "FOREIGN KEY (" + FK_ID_DESPESA + ") REFERENCES "+DbTableTipoDespesa.TABLE_NAME+"("+DbTableTipoDespesa.ID_DESPESA+")"+")"
-
+                "CREATE TABLE "+TABLE_NAME+" " +
+                        "("+
+                ID_MOVIMENTO+" TEXT PRIMARY KEY, "+
+                DIA+" INTEGER NOT NULL, "+
+                MES+" INTEGER NOT NULL, "+
+                ANO+" INTEGER NOT NULL, "+
+                RECEITADESPESA+" TEXT NOT NULL, "+
+                DESGINACAO+" TEXT NOT NULL, "+
+                VALOR+" REAL NOT NULL, "+
+                FK_ID_RECEITA+" TEXT REFERENCES "+DbTableTipoReceita.TABLE_NAME+" ("+DbTableTipoReceita.ID_RECEITA+"), "+
+                        FK_ID_DESPESA+" TEXT REFERENCES "+DbTableTipoDespesa.TABLE_NAME+" ("+DbTableTipoDespesa.ID_DESPESA+")" + ")"
         );
+    }
+
+    //CRUD
+    public static ContentValues getContentValues (RegistoMovimentos registoMovimentos){
+        ContentValues values = new ContentValues();
+        values.put(ID_MOVIMENTO, registoMovimentos.getId_movimento());
+        values.put(DIA, registoMovimentos.getDia());
+        values.put(MES, registoMovimentos.getMes());
+        values.put(ANO, registoMovimentos.getAno());
+        values.put(RECEITADESPESA, registoMovimentos.getReceitadespesa());
+        values.put(DESGINACAO, registoMovimentos.getDesignacao());
+        values.put(VALOR, registoMovimentos.getValor());
+        values.put(FK_ID_RECEITA, registoMovimentos.getTiporeceita());
+        values.put(FK_ID_DESPESA, registoMovimentos.getTipodespesa());
+
+        return values;
+    }
+
+    //insert
+    public long insert (ContentValues values){
+        return db.insert(TABLE_NAME, null, values);
     }
 
 
