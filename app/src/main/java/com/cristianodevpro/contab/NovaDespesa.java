aea10bb462f8ffc0aa145d9852750f1593151eaa
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-05 22:41:26 +0100

    Adicionada ação ao botão "definirData" - Date Picker Dialog

--- app/src/main/java/com/cristianodevpro/contab/NovaDespesa.java
+++ app/src/main/java/com/cristianodevpro/contab/NovaDespesa.java
@@ -1,14 +1,23 @@
 package com.cristianodevpro.contab;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class NovaDespesa extends AppCompatActivity {
+import java.util.Calendar;
 
+public class NovaDespesa extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private static Boolean isClicked = false;
+
+    RegistoMovimentos registoMovimentos = new RegistoMovimentos();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +35,28 @@ public class NovaDespesa extends AppCompatActivity {
     }
 
     public void definirDataDespesa(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        //Código que obtém a data selecionada pelo utilizador
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        month++;
+
+        registoMovimentos.setDia(dayOfMonth);
+        registoMovimentos.setMes(month);
+        registoMovimentos.setAno(year);
+
+        TextView textViewSelectedDateDespesa = (TextView) findViewById(R.id.textViewSelectedDateDespesa);
+        textViewSelectedDateDespesa.setText(""+dayOfMonth+"/"+month+"/"+year);
+
+        isClicked = true;
     }
 }
