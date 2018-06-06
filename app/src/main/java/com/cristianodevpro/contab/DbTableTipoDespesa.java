03af2e292f083029550c8077083dc6f0dbd23bc1
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-06 22:48:35 +0100

    Adicionadas funcionalidades CRUD à tabela TipoDespesa

--- app/src/main/java/com/cristianodevpro/contab/DbTableTipoDespesa.java
+++ app/src/main/java/com/cristianodevpro/contab/DbTableTipoDespesa.java
@@ -1,14 +1,17 @@
 package com.cristianodevpro.contab;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
 public class DbTableTipoDespesa implements BaseColumns {
 
     public static final String TABLE_NAME = "TipoDespesa";
-    public static final String ID_DESPESA = "id_despesa";
+    //public static final int ID_DESPESA = "id_despesa";
     public static final String CATEGORIA_DESPESAS = "categoria";
+
+    public static final String[] ALL_COLUMNS = new String[]{_ID, CATEGORIA_DESPESAS};
     private SQLiteDatabase db;
 
     public DbTableTipoDespesa(SQLiteDatabase db) {
@@ -19,7 +22,7 @@ public class DbTableTipoDespesa implements BaseColumns {
     public void create(){
         db.execSQL(
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                ID_DESPESA + " TEXT PRIMARY KEY," +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 CATEGORIA_DESPESAS + " TEXT NOT NULL)"
         );
     }
@@ -27,13 +30,41 @@ public class DbTableTipoDespesa implements BaseColumns {
     //CRUD
     public static ContentValues getContentValues(TipoDespesa tipoDespesa){
         ContentValues values = new ContentValues();
-        values.put(ID_DESPESA, tipoDespesa.getId_despesa());
+        values.put(_ID, tipoDespesa.getId_despesa());
         values.put(CATEGORIA_DESPESAS, tipoDespesa.getCategoria());
 
         return values;
     }
 
+    public static TipoDespesa getCurrentTipoDespesaFromCursor(Cursor cursor){
+        final int posId = cursor.getColumnIndex(_ID);
+        final int posCatDes = cursor.getColumnIndex(CATEGORIA_DESPESAS);
+
+        TipoDespesa tipoDespesa = new TipoDespesa();
+
+        tipoDespesa.setId_despesa(cursor.getInt(posId));
+        tipoDespesa.setCategoria(cursor.getString(posCatDes));
+
+        return tipoDespesa;
+    }
+
+    //insert
     public long insert(ContentValues values) {
         return db.insert(TABLE_NAME, null, values);
+    }
+
+    //update
+    public int update(ContentValues values, String whereClause, String[] whereArgs){
+        return db.update(TABLE_NAME,values,whereClause,whereArgs);
+    }
+
+    //delete
+    public int delete(String whereClause, String[] whereArgs){
+        return db.delete(TABLE_NAME,whereClause,whereArgs);
+    }
+
+    //read
+    public Cursor query (String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy){
+        return db.query(TABLE_NAME, columns, selection, selectionArgs, groupBy, having, orderBy);
     }
 }
