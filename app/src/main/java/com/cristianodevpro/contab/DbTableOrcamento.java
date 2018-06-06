3265541555d86864c633dead23bb1e46ce69ec97
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-06 19:50:52 +0100

    Adicionadas funcionalidades CRUD à tabela Orcamento

--- app/src/main/java/com/cristianodevpro/contab/DbTableOrcamento.java
+++ app/src/main/java/com/cristianodevpro/contab/DbTableOrcamento.java
@@ -3,6 +3,8 @@ package com.cristianodevpro.contab;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+import android.widget.Toolbar;
+import android.database.Cursor;
 
 import java.util.Locale;
 
@@ -11,6 +13,8 @@ public class DbTableOrcamento implements BaseColumns {
     public static final String ID_ORCAMENTO = "id_orcamento";
     public static final String VALOR = "valor";
     public static final String TABLE_NAME = "Orcamento";
+
+    public static final String[] ALL_COLUMNS = new String[]{ID_ORCAMENTO, VALOR};
     private SQLiteDatabase db;
 
     public DbTableOrcamento(SQLiteDatabase db) {
@@ -35,8 +39,37 @@ public class DbTableOrcamento implements BaseColumns {
         return values;
     }
 
-    //inserir
+    public static Orcamento getCurrentOrcamentoFromCursor(Cursor cursor){
+        final int posId = cursor.getColumnIndex(ID_ORCAMENTO);
+        final int posValor = cursor.getColumnIndex(VALOR);
+
+        Orcamento orcamento = new Orcamento();
+
+        orcamento.setId_orcamento(cursor.getString(posId));
+        orcamento.setValor(cursor.getDouble(posValor));
+
+        return orcamento;
+    }
+
+    //create
     public long insert(ContentValues values){
         return db.insert(TABLE_NAME, null, values);
     }
+
+    //update
+    public int update(ContentValues values, String whereClause, String[] whereArgs){
+        return db.update(TABLE_NAME,values,whereClause,whereArgs);
+    }
+
+    //delete
+    public int delete(String whereClause, String[] whereArgs){
+        return db.delete(TABLE_NAME,whereClause,whereArgs);
+    }
+
+    //read
+    public Cursor query (String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy){
+        return db.query(TABLE_NAME, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
+
 }
