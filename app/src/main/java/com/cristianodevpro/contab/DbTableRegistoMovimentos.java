4e229cb727fefbbd3f1379e0bb9bc53091c11cc3
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-13 11:26:10 +0100

    Adicionadas funções "getValorDespesasFromDb(Cursor cursor)" e "getValorReceitasFromDb(Cursor cursor)" à classe DbTableRegistoMovimentos

--- app/src/main/java/com/cristianodevpro/contab/DbTableRegistoMovimentos.java
+++ app/src/main/java/com/cristianodevpro/contab/DbTableRegistoMovimentos.java
@@ -1,6 +1,7 @@
 package com.cristianodevpro.contab;
 
 import android.content.ContentValues;
+import android.database.CrossProcessCursor;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
@@ -8,7 +9,7 @@ import android.provider.BaseColumns;
 public class DbTableRegistoMovimentos implements BaseColumns {
 
     public static final String TABLE_NAME = "RegistoMovimentos";
-    public static final String ID_MOVIMENTO = "id_movimento";
+    public static final String _ID = "id_movimento";
     public static final String DIA = "dia";
     public static final String MES = "mes";
     public static final String ANO = "ano";
@@ -18,7 +19,10 @@ public class DbTableRegistoMovimentos implements BaseColumns {
     public static final String FK_ID_RECEITA = "id_Receita";
     public static final String FK_ID_DESPESA = "id_Despesa";
 
-    public static final String[] ALL_COLUMNS = new String[]{ID_MOVIMENTO, DIA, MES, ANO, RECEITADESPESA, DESGINACAO, VALOR, FK_ID_RECEITA, FK_ID_DESPESA};
+    public static final String[] ALL_COLUMNS = new String[]{_ID, DIA, MES, ANO, RECEITADESPESA, DESGINACAO, VALOR, FK_ID_RECEITA, FK_ID_DESPESA};
+    public static final String[] INSERT_RECEITA_COLUMNS = new String[]{_ID, DIA, MES, ANO, RECEITADESPESA, DESGINACAO, VALOR, FK_ID_RECEITA};
+    public static final String[] INSERT_DESPESA_COLUMNS = new String[]{_ID, DIA, MES, ANO, RECEITADESPESA, DESGINACAO, VALOR, FK_ID_DESPESA};
+    public static final String[] VALOR_COLUMN = new String[]{VALOR};
 
     private SQLiteDatabase db;
 
@@ -32,22 +36,22 @@ public class DbTableRegistoMovimentos implements BaseColumns {
         db.execSQL(
                 "CREATE TABLE "+TABLE_NAME+" " +
                         "("+
-                ID_MOVIMENTO+" TEXT PRIMARY KEY, "+
+                _ID+" TEXT PRIMARY KEY, "+
                 DIA+" INTEGER NOT NULL, "+
                 MES+" INTEGER NOT NULL, "+
                 ANO+" INTEGER NOT NULL, "+
                 RECEITADESPESA+" TEXT NOT NULL, "+
                 DESGINACAO+" TEXT, "+
                 VALOR+" REAL NOT NULL, "+
-                FK_ID_RECEITA+" INTEGER REFERENCES "+DbTableTipoReceita.TABLE_NAME+" ("+DbTableTipoReceita.ID_RECEITA+"), "+
-                        FK_ID_DESPESA+" INTEGER REFERENCES "+DbTableTipoDespesa.TABLE_NAME+" ("+DbTableTipoDespesa.ID_DESPESA+")" + ")"
+                FK_ID_RECEITA+" INTEGER REFERENCES "+DbTableTipoReceita.TABLE_NAME+" ("+DbTableTipoReceita._ID+"), "+
+                        FK_ID_DESPESA+" INTEGER REFERENCES "+DbTableTipoDespesa.TABLE_NAME+" ("+DbTableTipoDespesa._ID+")" + ")"
         );
     }
 
     //CRUD
     public static ContentValues getContentValues (RegistoMovimentos registoMovimentos){
         ContentValues values = new ContentValues();
-        values.put(ID_MOVIMENTO, registoMovimentos.getId_movimento());
+        values.put(_ID, registoMovimentos.getId_movimento());
         values.put(DIA, registoMovimentos.getDia());
         values.put(MES, registoMovimentos.getMes());
         values.put(ANO, registoMovimentos.getAno());
@@ -60,15 +64,14 @@ public class DbTableRegistoMovimentos implements BaseColumns {
         return values;
     }
 
-    public static RegistoMovimentos getCurrentTipoDespesaRegistoMovimentoFromCursor(Cursor cursor){
-        final int posId = cursor.getColumnIndex(ID_MOVIMENTO);
+    public static RegistoMovimentos getCurrentRegistoMovimentoDespesaFromCursor(Cursor cursor){
+        final int posId = cursor.getColumnIndex(_ID);
         final int posDia = cursor.getColumnIndex(DIA);
         final int posMes = cursor.getColumnIndex(MES);
         final int posAno = cursor.getColumnIndex(ANO);
         final int posRecDes = cursor.getColumnIndex(RECEITADESPESA);
         final int posDesig = cursor.getColumnIndex(DESGINACAO);
         final int posValor = cursor.getColumnIndex(VALOR);
-        final int posIdRec = cursor.getColumnIndex(FK_ID_RECEITA);
         final int posIdDes = cursor.getColumnIndex(FK_ID_DESPESA);
 
         RegistoMovimentos registoMovimentos = new RegistoMovimentos();
@@ -80,10 +83,61 @@ public class DbTableRegistoMovimentos implements BaseColumns {
         registoMovimentos.setReceitadespesa(cursor.getString(posRecDes));
         registoMovimentos.setDesignacao(cursor.getString(posDesig));
         registoMovimentos.setValor(cursor.getDouble(posValor));
-        registoMovimentos.setTiporeceita(cursor.getInt(posIdRec));
         registoMovimentos.setTipodespesa(cursor.getInt(posIdDes));
 
         return registoMovimentos;
+    }
+
+    public static RegistoMovimentos getCurrentRegistoMovimentoReceitaFromCursor(Cursor cursor){
+        final int posId = cursor.getColumnIndex(_ID);
+        final int posDia = cursor.getColumnIndex(DIA);
+        final int posMes = cursor.getColumnIndex(MES);
+        final int posAno = cursor.getColumnIndex(ANO);
+        final int posRecDes = cursor.getColumnIndex(RECEITADESPESA);
+        final int posDesig = cursor.getColumnIndex(DESGINACAO);
+        final int posValor = cursor.getColumnIndex(VALOR);
+        final int posIdRec = cursor.getColumnIndex(FK_ID_RECEITA);
+
+        RegistoMovimentos registoMovimentos = new RegistoMovimentos();
+
+        registoMovimentos.setId_movimento(cursor.getString(posId));
+        registoMovimentos.setDia(cursor.getInt(posDia));
+        registoMovimentos.setMes(cursor.getInt(posMes));
+        registoMovimentos.setAno(cursor.getInt(posAno));
+        registoMovimentos.setReceitadespesa(cursor.getString(posRecDes));
+        registoMovimentos.setDesignacao(cursor.getString(posDesig));
+        registoMovimentos.setValor(cursor.getDouble(posValor));
+        registoMovimentos.setTiporeceita(cursor.getInt(posIdRec));
+
+        return registoMovimentos;
+    }
+
+    public static double getValorDespesasFromDb(Cursor cursor){ //Obter o valor das despesas
+       final int posValor = cursor.getColumnIndex("valor");
+
+        double totalDespesas = 0;
+        double valor = 0;
+        if (cursor.getCount() > 0){ //Encontra pelo menos um registo
+            cursor.moveToFirst();
+            while (cursor.moveToNext()) {
+                valor = cursor.getDouble(posValor);
+                totalDespesas = totalDespesas + valor;
+            }
+        }
+
+        return totalDespesas;
+    }
+
+    public static double getValorReceitasFromDb(Cursor cursor){
+        final int posValor = 0; //select SUM(valor) -> coluna 0
+
+        double valorReceitas = 0;
+        if (cursor.getCount() > 0){ //Encontra um registo
+            cursor.moveToFirst();
+            valorReceitas = cursor.getDouble(posValor);
+        }
+
+        return valorReceitas;
     }
 
     //insert
