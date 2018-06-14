42703b69d77bda06047e6181eb8e63cc577577a6
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-14 20:05:37 +0100

    Adicionada classe DadosDefinicoesToMain Tem por objetivo guardar os dados definidos na atividade Definicoes para depois obtê-los na atividade Main

---
+++ app/src/main/java/com/cristianodevpro/contab/DadosDefinicoesToMain.java
@@ -0,0 +1,42 @@
+package com.cristianodevpro.contab;
+
+public class DadosDefinicoesToMain {
+    private static int dia;
+    private static int mes;
+    private static int ano;
+    private static String tipo;
+
+    public static String getTipo() {
+        return tipo;
+    }
+
+    public static void setTipo(String tipo) {
+        DadosDefinicoesToMain.tipo = tipo;
+    }
+
+    public static int getDia() {
+        return dia;
+    }
+
+    public static void setDia(int dia) {
+        DadosDefinicoesToMain.dia = dia;
+    }
+
+    public static int getMes() {
+        return mes;
+    }
+
+    public static void setMes(int mes) {
+        DadosDefinicoesToMain.mes = mes;
+    }
+
+    public static int getAno() {
+        return ano;
+    }
+
+    public static void setAno(int ano) {
+        DadosDefinicoesToMain.ano = ano;
+    }
+
+
+}
