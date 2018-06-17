cfffceeeffab5a03d39c084946715e4c8d9a0a5b
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-17 18:53:55 +0100

    Adicionada Classe EditReceita

---
+++ app/src/main/java/com/cristianodevpro/contab/EditReceita.java
@@ -0,0 +1,13 @@
+package com.cristianodevpro.contab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditReceita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_receita);
+    }
+}
