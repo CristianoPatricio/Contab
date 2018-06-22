9da8f88ccc55dcaac86a7d2af4acfcb197353f9c
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-22 22:50:05 +0100

    Adicionado Content Provider

---
+++ app/src/main/java/com/cristianodevpro/contab/ContabContentProvider.java
@@ -0,0 +1,81 @@
+package com.cristianodevpro.contab;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class ContabContentProvider extends ContentProvider {
+    /**
+     * @return
+     */
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    /**
+     *
+     * @param uri
+     * @param strings
+     * @param s
+     * @param strings1
+     * @param s1
+     * @return
+     */
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    /**
+     *
+     * @param uri
+     * @return
+     */
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    /**
+     *
+     * @param uri
+     * @param contentValues
+     * @return
+     */
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    /**
+     *
+     * @param uri
+     * @param s
+     * @param strings
+     * @return
+     */
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    /**
+     *
+     * @param uri
+     * @param contentValues
+     * @param s
+     * @param strings
+     * @return
+     */
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
