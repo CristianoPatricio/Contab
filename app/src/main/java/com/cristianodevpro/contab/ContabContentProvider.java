1bc58be0416e733b1b8f94b2cdfa5bb4ac3880f2
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-22 23:03:26 +0100

    Implementada query (Contab Content Provider)

--- app/src/main/java/com/cristianodevpro/contab/ContabContentProvider.java
+++ app/src/main/java/com/cristianodevpro/contab/ContabContentProvider.java
@@ -2,13 +2,28 @@ package com.cristianodevpro.contab;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class ContabContentProvider extends ContentProvider {
+
+    private static final int CATEGORIAS_RECEITAS = 200;
+    private static final int CATEGORIAS_RECEITAS_ID = 201;
+
     DbContabOpenHelper dbContabOpenHelper;
+
+    private static UriMatcher getContabUriMatcher(){
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI("com.cristianodevpro.contab", "categorias_receitas", CATEGORIAS_RECEITAS);
+        uriMatcher.addURI("com.cristianodevpro.contab", "categorias_receitas/#", CATEGORIAS_RECEITAS_ID);
+
+        return uriMatcher;
+    }
     /**
      * @return
      */
@@ -21,16 +36,32 @@ public class ContabContentProvider extends ContentProvider {
     /**
      *
      * @param uri
-     * @param strings
-     * @param s
-     * @param strings1
-     * @param s1
+     * @param projection
+     * @param selection
+     * @param selectionArgs
+     * @param sortOrder
      * @return
      */
     @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        SQLiteDatabase db = dbContabOpenHelper.getReadableDatabase();
+
+        String id = uri.getLastPathSegment();
+
+        UriMatcher matcher = getContabUriMatcher();
+
+        switch (matcher.match(uri)){
+
+            case CATEGORIAS_RECEITAS:
+                return new DbTableTipoReceita(db).query(projection,selection,selectionArgs,null,null,sortOrder);
+
+                case CATEGORIAS_RECEITAS_ID:
+                    return new DbTableTipoReceita(db).query(projection, DbTableTipoReceita._ID+"=?",new String[]{id},null,null,sortOrder);
+
+                default:
+                    throw new UnsupportedOperationException("Invalid URI: " + uri);
+        }
     }
 
     /**
