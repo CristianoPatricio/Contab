5f6b2ba3c0c70f50dd16709e834f47ff0da9c0d6
Cristiano Patrício <cristianop1998@gmail.com>
2018-06-30 00:10:34 +0100

    Adicionados botões para inserir categorias [Class GerirCategorias]. Testes OK!

--- app/src/main/java/com/cristianodevpro/contab/GerirCategorias.java
+++ app/src/main/java/com/cristianodevpro/contab/GerirCategorias.java
@@ -16,7 +16,10 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class GerirCategorias extends AppCompatActivity {
+public class GerirCategorias extends AppCompatActivity implements DialogFragmentCategoria.ExampleDialogListener {
+
+    public static boolean clickedButtonCatReceita = false;
+    public static boolean clickButtonCatDespesa = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,6 +103,64 @@ public class GerirCategorias extends AppCompatActivity {
                 /**************************************************************************************/
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        clickedButtonCatReceita = false;
+        clickButtonCatDespesa = false;
+    }
+
+    /***************************************Buttons actions**********************************************/
+
+    public void addCategoriaReceita(View view) { //Botão "adicionar Categoria Receita"
+        clickedButtonCatReceita = true;
+        DialogFragmentCategoria dialogFragmentCategoria = new DialogFragmentCategoria();
+        dialogFragmentCategoria.show(getSupportFragmentManager(), "DialogFragmentCategoria");
+    }
+
+    public void addCategoriaDespesa(View view) { //Botão "adicionar Categoria Despesa"
+        clickButtonCatDespesa = true;
+        DialogFragmentCategoria dialogFragmentCategoria = new DialogFragmentCategoria();
+        dialogFragmentCategoria.show(getSupportFragmentManager(), "DialogFragmentCategoria");
+    }
+
+    @Override
+    public void setTexts(String categoria) {
+
+        if (clickedButtonCatReceita && !clickButtonCatDespesa) { //Click no Botão add cat. receitas
+            try {
+                if (checkCategoriaReceita(categoria) != -1) { //Se devolver um id != -1 é porque já exite uma categoria com o nome que vamos inserir
+                    Toast.makeText(GerirCategorias.this, "Categoria já existente!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                insertCategoriaReceitaDb(categoria);
+                Toast.makeText(GerirCategorias.this, "Categoria inserida com sucesso!", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(GerirCategorias.this, "Erro ao inserir a categoria na BD!", Toast.LENGTH_LONG).show();
+            }
+
+            updateListCategoriasReceitas(); //atualizar listview cat. receitas
+
+            clickedButtonCatReceita = false;
+        }else if (clickButtonCatDespesa && !clickedButtonCatReceita){ //Click no Botão ass cat. despesas
+            try {
+                if (checkCategoriaDespesa(categoria) != -1) { //Se devolver um id != -1 é porque já exite uma categoria com o nome que vamos inserir
+                    Toast.makeText(GerirCategorias.this, "Categoria já existente!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                insertCategoriaDespesaDb(categoria);
+                Toast.makeText(GerirCategorias.this, "Categoria inserida com sucesso!", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(GerirCategorias.this, "Erro ao inserir a categoria na BD!", Toast.LENGTH_LONG).show();
+            }
+
+            updateListCategoriasDespesas(); //atualizar listview cat. despesas
+
+            clickButtonCatDespesa = false;
+        }
+
     }
 
     /**********************************Functions and Methods***************************************/
@@ -187,7 +248,7 @@ public class GerirCategorias extends AppCompatActivity {
         //Abrir a BD
         DbContabOpenHelper dbContabOpenHelper = new DbContabOpenHelper(getApplicationContext());
         //Escrita
-        SQLiteDatabase db = dbContabOpenHelper.getReadableDatabase();
+        SQLiteDatabase db = dbContabOpenHelper.getWritableDatabase();
 
         DbTableTipoDespesa dbTableTipoDespesa = new DbTableTipoDespesa(db);
         dbTableTipoDespesa.delete(DbTableTipoDespesa._ID+"=?",new String[]{Integer.toString(id)});
@@ -208,4 +269,77 @@ public class GerirCategorias extends AppCompatActivity {
         ListView listViewDespesas = (ListView) findViewById(R.id.listViewDespesas);
         listViewDespesas.setAdapter(listAdapterDespesas);
     }
+
+    private void insertCategoriaReceitaDb(String categoria) {
+        //Abrir BD
+        DbContabOpenHelper dbContabOpenHelper = new DbContabOpenHelper(getApplicationContext());
+        //Op. escrita
+        SQLiteDatabase db = dbContabOpenHelper.getWritableDatabase();
+
+        DbTableTipoReceita tableTipoReceita = new DbTableTipoReceita(db);
+
+        TipoReceita tipoReceita = new TipoReceita();
+        tipoReceita.setCategoria(categoria);
+
+        tableTipoReceita.insert(DbTableTipoReceita.getContentValues(tipoReceita));
+        db.close();
+    }
+
+    public int checkCategoriaReceita(String categoria){
+        //Abrir a BD
+        DbContabOpenHelper dbContabOpenHelper = new DbContabOpenHelper(getApplicationContext());
+        //Escrita
+        SQLiteDatabase db = dbContabOpenHelper.getReadableDatabase();
+
+        String query = "SELECT "+DbTableTipoReceita._ID+" FROM "+DbTableTipoReceita.TABLE_NAME+" WHERE "+DbTableTipoReceita.CATEGORIA_RECEITA+" =?";
+        Cursor cursor = db.rawQuery(query,new String[]{categoria});
+
+        int id = -1;
+
+        if (cursor.getCount() > 0){ //Se devolver pelo menos uma linha é porque a categoria já existe.
+            cursor.moveToFirst();
+            id = cursor.getInt(cursor.getColumnIndex(DbTableTipoReceita._ID));
+        }
+
+        cursor.close();
+        db.close();
+        return id;
+    }
+
+    private void insertCategoriaDespesaDb(String categoria) {
+        //Abrir BD
+        DbContabOpenHelper dbContabOpenHelper = new DbContabOpenHelper(getApplicationContext());
+        //Op. escrita
+        SQLiteDatabase db = dbContabOpenHelper.getWritableDatabase();
+
+        DbTableTipoDespesa tableTipoDespesa = new DbTableTipoDespesa(db);
+
+        TipoDespesa tipoDespesa = new TipoDespesa();
+        tipoDespesa.setCategoria(categoria);
+
+        tableTipoDespesa.insert(DbTableTipoDespesa.getContentValues(tipoDespesa));
+        db.close();
+    }
+
+    public int checkCategoriaDespesa(String categoria){//retorna id da categoria
+        //Abrir a BD
+        DbContabOpenHelper dbContabOpenHelper = new DbContabOpenHelper(getApplicationContext());
+        //Escrita
+        SQLiteDatabase db = dbContabOpenHelper.getReadableDatabase();
+
+        String query = "SELECT "+DbTableTipoDespesa._ID+" FROM "+DbTableTipoDespesa.TABLE_NAME+" WHERE "+DbTableTipoDespesa.CATEGORIA_DESPESAS+" =?";
+        Cursor cursor = db.rawQuery(query,new String[]{categoria});
+
+        int id = -1;
+
+        if (cursor.getCount() > 0){ //Se devolver pelo menos uma linha é porque a categoria já existe.
+            cursor.moveToFirst();
+            id = cursor.getInt(cursor.getColumnIndex(DbTableTipoDespesa._ID));
+        }
+
+        cursor.close();
+        db.close();
+        return id;
+    }
+
 }
