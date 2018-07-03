9e7ac7eadb09a6bd974c4722d3fa0eef97eeaae6
Cristiano Patrício <cristianop1998@gmail.com>
2018-07-03 18:57:00 +0100

    Class Sobre Final

--- app/src/main/java/com/cristianodevpro/contab/Sobre.java
+++ app/src/main/java/com/cristianodevpro/contab/Sobre.java
@@ -1,7 +1,9 @@
 package com.cristianodevpro.contab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sobre extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class Sobre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Take care of popping the fragment back stack or finishing the activity
+     * as appropriate.
+     */
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    /************************Functions***************************/
+
+    /**
+     * preencher campos TO, SUBJECT, TEXT de uma mensagem de correio eletrónico
+     *
+     * @param view
+     */
+    public void sendEmail(View view) {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_TEXT, "A sua mensagem de texto...");
+        i.putExtra(Intent.EXTRA_EMAIL, "dicasdeinformatica2015@gmail.com");
+        i.putExtra(Intent.EXTRA_SUBJECT, "Suporte Contab");
+        i.setType("text/plain");
+        startActivity(i);
     }
 }
