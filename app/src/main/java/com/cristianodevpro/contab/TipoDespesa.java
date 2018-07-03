3f764a8272486ca011c37f3bafee130be45a2b77
Cristiano Patrício <cristianop1998@gmail.com>
2018-07-03 19:09:22 +0100

    Class TipoDespesa Final

--- app/src/main/java/com/cristianodevpro/contab/TipoDespesa.java
+++ app/src/main/java/com/cristianodevpro/contab/TipoDespesa.java
@@ -1,22 +1,24 @@
 package com.cristianodevpro.contab;
 
 public class TipoDespesa {
-    String id_despesa, categoria;
+    int id_despesa;
+    String categoria;
 
     //Construtores
     public TipoDespesa() {}
 
-    public TipoDespesa(String id_despesa, String categoria){
+
+    public TipoDespesa(int id_despesa, String categoria){
         this.id_despesa = id_despesa;
         this.categoria = categoria;
     }
 
     //Setters and Getters
-    public String getId_despesa() {
+    public int getId_despesa() {
         return id_despesa;
     }
 
-    public void setId_despesa(String id_despesa) {
+    public void setId_despesa(int id_despesa) {
         this.id_despesa = id_despesa;
     }
 
