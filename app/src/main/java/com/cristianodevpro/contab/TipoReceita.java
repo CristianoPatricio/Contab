1867d715c96cdd4ba2ca82bf777357ed0f601136
Cristiano Patrício <cristianop1998@gmail.com>
2018-07-03 19:09:39 +0100

    Class TipoReceita Final

--- app/src/main/java/com/cristianodevpro/contab/TipoReceita.java
+++ app/src/main/java/com/cristianodevpro/contab/TipoReceita.java
@@ -1,21 +1,23 @@
 package com.cristianodevpro.contab;
 
 public class TipoReceita {
-    String id_receita, categoria;
+    int id_receita;
+    String categoria;
 
     //Construtores
     public TipoReceita() {}
-    public TipoReceita(String id_receita, String categoria) {
+
+    public TipoReceita(int id_receita, String categoria) {
         this.id_receita = id_receita;
         this.categoria = categoria;
     }
 
     //Setters and Getters
-    public String getId_receita() {
+    public int getId_receita() {
         return id_receita;
     }
 
-    public void setId_receita(String id_receita) {
+    public void setId_receita(int id_receita) {
         this.id_receita = id_receita;
     }
 
